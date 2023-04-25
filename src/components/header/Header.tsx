268bc287a85b5813a7d0e65bd4c28b093bf1e8ee
salino3 <flaviobcn012@gmail.com>
2023-04-25 09:47:22 +0200

    fix icon navbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,15 +6,14 @@ export const Header: React.FC = () => {
   return (
     <header>
       <div>
-        <img src="../../../public/assets/shape-lines.png" alt="menu" />
+        <img src="/assets/shape-lines.png" alt="menu" />
       </div>
       <div className="container_icon">
-        <img className="img_1" src="/assets/O.png" alt="O" />
-        <img className="img_2" src="/assets/mix.png" alt="mix" />
-        <img className="img_3" src="/assets/Line-1-Stroke.png" alt="line" />
+        <img className="img_1" src="/assets/logo.svg" alt="logo" />
+       
       </div>
       <Navbar />
-      <div>
+      <div className="boxImages">
         <img src="/assets/shape-2.png" alt="magnifying glass" />
         <img src="/assets/shape-heart.png" alt="heart" />
         <img src="/assets/shape.png" alt="cart" />
