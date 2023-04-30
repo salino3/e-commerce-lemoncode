6348b2ce2f27b17dd14ad71f6cdb0a96ab48ed11
salino3 <flaviobcn012@gmail.com>
2023-04-30 15:49:53 +0200

    fix styles third page

--- src/components-third-page/main-component-ismobile/main-component-ismobile.tsx
+++ src/components-third-page/main-component-ismobile/main-component-ismobile.tsx
@@ -1,34 +1,10 @@
 import React from 'react';
 import * as classes from "./main-ismobile.style";
-import { ListComponent } from '../../components-second-page';
-
-
-interface PropsImg {
-  img1: boolean;
-  img2: boolean;
-  img3: boolean;
-  img4: boolean;
-  img5: boolean;
-  [index: string]: boolean;
-};
+import { Button } from '../../commons';
 
 
 export const MainComponentIsmobile: React.FC = () => {
 
-  // const [toggleIcon, setToggleIcon] = React.useState<PropsImg>({
-  //   img1: true,
-  //   img2: false,
-  //   img3: false,
-  //   img4: false,
-  //   img5: false,
-  // });
-
-  // function handleClick(index: string) {
-  //   setToggleIcon((prevState) => ({
-  //     ...prevState,
-  //     [index]: !prevState[index],
-  //   }));
-  // }
   return (
     <>
       <div className={classes.boxIsMobile}>
@@ -55,60 +31,57 @@ export const MainComponentIsmobile: React.FC = () => {
             <img src="/assets/small-image-second-page2.png" alt="image" />
             <img src="/assets/small-image-second-page3.png" alt="image" />
           </div>
-        </div>
-        {/* <div className={classes.boxImages}>
-          <div className={classes.boxPrimaryImages}>
-            <img src="/assets/primaryImage.png" alt="image" /> <br />
-            <div>
-              <img src="/assets/small-image-second-page1.png" alt="image" />
-              <img src="/assets/small-image-second-page2.png" alt="image" />
-              <img src="/assets/small-image-second-page3.png" alt="image" />
+          <div>
+            <div className={classes.btn}>
+              Color
+              <svg
+                width="10"
+                height="7"
+                viewBox="0 0 10 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.833344 1.33325L5.00001 5.49992L9.16668 1.33325"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <div className={classes.btn}>
+              XS
+              <svg
+                width="10"
+                height="7"
+                viewBox="0 0 10 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.833344 1.33325L5.00001 5.49992L9.16668 1.33325"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </div>
           </div>
-          <div className={classes.boxSecondaryImages}>
-            <div>
-              <img src="/assets/star.png" alt="star" />
-              <img src="/assets/star.png" alt="star" />
-              <img src="/assets/star.png" alt="star" />
-              <img src="/assets/star.png" alt="star" />
-              <img src="/assets/star.png" alt="star" />
-            </div>
-            <div className={classes.title}>
-              <h2> Vaqueros largos rectos en blanco</h2>
-              <div>
-                <img src="/assets/shape-hearth.png" alt="hearth" /> &nbsp;
-                <img src="/assets/shareIcon.png" alt="star" />
-              </div>
-            </div>
+          <div>
+            <Button className={classes.lightButton}>
+              <img src={`/assets/icon-minus.png`} alt="icon" />
+              1
+              <img src={`/assets/icon-plus.png`} alt="icon" />
+            </Button>
             <div className={classes.price}>79.99 €</div>
-            <div className={classes.colores}>
-              Color:
-              <div></div>
-            </div>
-            <br />
-            <div className={classes.size}>
-              Talla:
-              <div>
-                <div>XS</div>
-                <div>S</div>
-                <div>M</div>
-                <div>L</div>
-              </div>
-            </div>
-            <img src="/assets/group143.png" alt="image" />
-            <div className={classes.boxButtons}>
-              <Button className={classes.lightButton}>
-                <img src={`/assets/icon-minus.png`} alt="icon" />
-                1
-                <img src={`/assets/icon-plus.png`} alt="icon" />
-              </Button>
-              <Button className={classes.darkButton}>
-                <img src="/assets/cartIcon.png" alt="image" />
-                &nbsp; En cesta
-              </Button>
-            </div>
           </div>
-        </div> */}
+          <Button className={classes.darkButton}>
+            <img src="/assets/cartIcon.png" alt="image" />
+            &nbsp; En cesta
+          </Button>
+        </div>
       </div>
     </>
   );
--- src/components-third-page/main-component-ismobile/main-ismobile.style.ts
+++ src/components-third-page/main-component-ismobile/main-ismobile.style.ts
@@ -1,17 +1,11 @@
 import { css } from "@emotion/css";
 
-// export const container = css`
-//  padding: 1rem;
-// `;
-
-
-
 export const boxIsMobile = css`
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 1rem;
-`;
+  `;
 
 
 export const title = css`
@@ -22,19 +16,18 @@ export const title = css`
   font-weight: 700;
   font-size: 24px;
   line-height: 120%;
-
+  
   color: #212429;
-`;
+  `;
 
 export const boxImages = css`
  
  display: flex;
  flex-direction: row;
  justify-content: space-around;
-`;
+ `;
 
 export const boxPrimaryImages = css`
-
   margin: auto;
 
   div {
@@ -43,4 +36,75 @@ export const boxPrimaryImages = css`
     flex-direction: row;
     justify-content: space-between;
   }
+`;
+
+
+
+export const btn = css`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+  border: solid;
+  box-sizing: border-box;
+
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+
+  color: #000000;
+
+  width: 152px;
+  height: 48px;
+  padding: 0rem 0.5rem;
+
+  background: #ffffff;
+  border: 1px solid #f7f7f7;
+  border-radius: 48px;
+`;
+
+
+export const lightButton = css`
+  display: flex;
+  flex-direction: row;
+  place-content: center;
+  justify-content: space-around;
+  align-items: center;
+  width: 176px;
+  height: 48px;
+ 
+
+  border: 1px solid #212429;
+  border-radius: 48px;
+`;
+
+export const price = css`
+
+  margin-right: 3rem; 
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 120%;
+  text-align: right;
+  color: #000000;
+  padding-right:  2rem;
+`;
+
+export const darkButton = css`
+  width: 296px;
+  height: 48px;
+  background: #212429;
+  border-radius: 48px;
+  width: 100%;
+  margin: 1rem 0rem;
+
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 150%;
+  color: #ffffff;
 `;
