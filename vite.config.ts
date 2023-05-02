bd631b8b34ad63a751e1a674fc8f9811b0e0f234
salino3 <flaviobcn012@gmail.com>
2023-05-02 11:09:48 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+ 
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
