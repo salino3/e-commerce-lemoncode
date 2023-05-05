6dc111577bd7928fafb2ec4761319daa1bd489cb
salino3 <flaviobcn012@gmail.com>
2023-05-05 19:09:43 +0200

    add card images

--- src/pods/payment/payment.component.tsx
+++ src/pods/payment/payment.component.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import * as classes from './payment.styles';
 
 export const PaymentComponent: React.FC = () => {
   return (
-    <div>payment.component: React.FC</div>
-  )
-}
+    <div className={classes.container}>
+      <div className={classes.boxText}>
+        <div className={classes.txtRoute}>Inicio/Cesta de la compra/Pedidos/Pago</div>
+        <h1 className={classes.title}>
+          <div>
+            <img src="/assets/shape-arrow-left.png" alt="arrow-left" />
+          </div>
+          Orden de pago
+        </h1>
+        <div className={classes.description}>Todas las transacciones son seguras y están encriptadas</div>
+        <div className={classes.boxImgs}>
+          <img className={classes.card1} src="../../../public/assets/card1.png" alt="card" />
+          <img className={classes.card2} src="../../../public/assets/card2.png" alt="card" />
+        </div>
+      </div >
+      <div className={classes.boxForm}>
+
+        <h2 className={classes.subTitle}>
+          Tarjeta de crédito
+        </h2>
+      </div>
+
+    </div>
+  );
+};
--- src/pods/payment/payment.styles.ts
+++ src/pods/payment/payment.styles.ts
@@ -1,2 +1,96 @@
 import {css} from '@emotion/css';
 
+
+export const container = css`
+  padding: 1rem;
+  margin-bottom: 5rem;
+
+`;
+
+export const boxText = css`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const txtRoute = css`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 130%;
+  color: #000000;
+`;
+
+export const title = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 120%;
+  font-variant: small-caps;
+  color: #000000;
+
+  img {
+    width: 16px;
+    height: 12px;
+    color: #000000;
+  }
+`;
+
+export const description = css`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  color: #999999;
+  margin-bottom: 2rem;
+`;
+
+export const boxImgs = css`
+
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ align-items: center;
+
+  img {
+    width: 85%;
+  }
+`;
+export const card1 = css`
+  position: relative;
+  right: 1rem;
+  z-index: 4;
+`;
+export const card2 = css`
+  position: relative;
+  left: 1rem;
+  z-index: 3;
+  bottom: 9rem;
+`;
+
+export const boxForm = css`
+
+
+ display: absolute;
+ border: solid;
+ top: 10rem;
+
+
+`
+
+export const subTitle = css`
+
+  display: absolute;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 150%;
+  color: #000000;
+`;
+
+
