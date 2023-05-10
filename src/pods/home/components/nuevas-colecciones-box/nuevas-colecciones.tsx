698f63b15cdbf2dbe5455a80f072e7ca2caf46f0
salino3 <flaviobcn012@gmail.com>
2023-05-10 14:46:43 +0200

    change px media query

--- src/pods/home/components/nuevas-colecciones-box/nuevas-colecciones.tsx
+++ src/pods/home/components/nuevas-colecciones-box/nuevas-colecciones.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 import { HomeCard } from '../card-home';
 import * as classes from './nueva-colecciones.styles';
